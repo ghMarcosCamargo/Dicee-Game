e661c585bc644409939eca4e96715b4cdbf2aa3e
ghMarcosCamargo <54373702+ghMarcosCamargo@users.noreply.github.com>
2020-03-18 12:41:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+
+document.querySelectorAll(".dice img")[0].src = "images/dice" + randomNumber1 + ".png";
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+document.querySelectorAll(".dice img")[1].src = "images/dice" + randomNumber2 + ".png";
+
+var winner;
+
+if(randomNumber1 > randomNumber2) {
+  winner = "🏆 Player 1 Wins!"
+} else {
+  if(randomNumber1 == randomNumber2) {
+    winner = "Draw!";
+  } else {
+    winner = "Player 2 Wins! 🏆";
+  }
+}
+
+document.getElementsByTagName("h1")[0].innerText= winner;
